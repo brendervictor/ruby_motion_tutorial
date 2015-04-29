c9fe7e3625ba0a2641f2ec6ade3eea087bc52a11
Victor Brender <victor@2xwide.com>
2015-04-29 17:35:52 +0200

    Initial commit.

---
+++ app/app_delegate.rb
@@ -0,0 +1,12 @@
+class AppDelegate
+  def application(application, didFinishLaunchingWithOptions:launchOptions)
+    # UIScreen describes the display our app is running on
+    @window = UIWindow.alloc.initWithFrame(UIScreen.mainScreen.bounds)
+    @window.makeKeyAndVisible
+
+    controller = TapController.alloc.initWithNibName(nil, bundle: nil)
+    @window.rootViewController = UINavigationController.alloc.initWithRootViewController(controller)
+
+    true
+  end
+end
